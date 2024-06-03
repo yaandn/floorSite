cbf913c18911cb00fd17199aa10cba357b62e10b
Yan Di Nápoli Araújo <dinapoliyan@gmail.com>
2024-06-03 16:35:51 -0300

    add images

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -9,6 +9,12 @@ import service6 from "../../assets/service6.jpg";
 import service7 from "../../assets/service7.jpg";
 import service8 from "../../assets/service8.jpg";
 import service9 from "../../assets/service9.jpg";
+import service10 from "../../assets/service10.jpg";
+import service11 from "../../assets/service11.jpg";
+import service12 from "../../assets/service12.jpg";
+import service13 from "../../assets/service13.jpg";
+import service14 from "../../assets/service14.jpg";
+import service15 from "../../assets/service15.jpg";
 
 export const Services = () => {
   return (
@@ -24,6 +30,12 @@ export const Services = () => {
         <img src={service7} alt="" />
         <img src={service8} alt="" />
         <img src={service9} alt="" />
+        <img src={service10} alt="" />
+        <img src={service11} alt="" />
+        <img src={service12} alt="" />
+        <img src={service13} alt="" />
+        <img src={service14} alt="" />
+        <img src={service15} alt="" />
       </div>
     </Container>
   );
